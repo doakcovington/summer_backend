bbb53e98b7b83637727f77fed60903bdd8ddeb94
doakcovington <doakcovington@protonmail.com>
2020-07-06 10:35:47 -0700

    Generated Category model and migration

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+hike = Category.create(name: "Hike")
+lake = Category.create(name: "Lake")
+golf = Category.create(name: "Golf")
