0e09c0aa06dc3ce796c4dae490efefd4e1209b28
doakcovington <doakcovington@protonmail.com>
2020-07-06 11:46:55 -0700

    Built out create method and post_params method

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -5,4 +5,19 @@ class Api::V1::PostsController < ApplicationController
         render json: posts
     end
 
+    def create
+        post = Post.new(post_params)
+        if post.save
+            render json: post, status: :accepted
+        else
+            render json: {errors: post.error.fullmessages}
+        end
+    end
+
+    private
+
+    def post_params
+        params.require(:post).permit(:title, :caption, :image_url, :category_id)
+    end
+
 end
